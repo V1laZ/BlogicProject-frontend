3d967cc623b7319cc92916c6be06acb5a026dec8
V1laZ <pasikstudiocz@gmail.com>
2022-08-22 14:03:52 +0200

    added csv export

--- src/app/poradci-table/poradci-table.component.ts
+++ src/app/poradci-table/poradci-table.component.ts
@@ -7,7 +7,7 @@ import { ApiService } from '../api.service';
   styleUrls: ['./poradci-table.component.css']
 })
 export class PoradciTableComponent implements OnInit {
-  poradci = [{id: '', jmeno: '', prijmeni: '', email: '', tel_cislo: '', rod_cislo: '', vek: ''}];
+  poradci = [{id: 0, jmeno: '', prijmeni: '', email: '', tel_cislo: 0, rod_cislo: '', vek: 0}];
 
   constructor(private api: ApiService) { }
 
@@ -22,6 +22,71 @@ export class PoradciTableComponent implements OnInit {
         error: (error) => (error.error['code'] == "token_not_valid") ? localStorage.removeItem('token') : ''
       }
     )
+  }
+
+  exportCsv() {
+    this.downloadFile(this.poradci);
+  }
+
+  strRep(data: any) {
+    if(typeof data == "string") {
+      let newData = data.replace(/,/g, " ");
+       return newData;
+    }
+    else if(typeof data == "undefined") {
+      return "-";
+    }
+    else if(typeof data == "number") {
+      return  data.toString();
+    }
+    else {
+      return data;
+    }
+  }
+
+  ConvertToCSV(objArray: string | { id: number; jmeno: string; prijmeni: string; email: string; tel_cislo: number; rod_cislo: string; vek: number; }[], headerList: string[]) {
+    console.log(objArray);
+    console.log(headerList);
+    let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+    let row = 'S.No,';
+
+    let newHeaders = ["id", "jmeno", "prijmeni", "email", "tel_cislo", "rod_cislo", "vek"];
+
+    for (let index in newHeaders) {
+      row += newHeaders[index] + ',';
+    }
+    row = row.slice(0, -1);
+    str += row + '\r\n';
+    for (let i = 0; i < array.length; i++) {
+      let line = (i + 1) + '';
+      for (let index in headerList) {
+        let head = headerList[index];
+
+        line += ',' + this.strRep(array[i][head]);
+      }
+      str += line + '\r\n';
+    }
+    return str;
+  }
+
+  downloadFile(data: { id: number; jmeno: string; prijmeni: string; email: string; tel_cislo: number; rod_cislo: string; vek: number; }[], filename = 'data') {
+    let arrHeader = ["id", "jmeno", "prijmeni", "email", "tel_cislo", "rod_cislo", "vek"];
+    let csvData = this.ConvertToCSV(data, arrHeader);
+    console.log(csvData)
+    let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
+    let dwldLink = document.createElement("a");
+    let url = URL.createObjectURL(blob);
+    let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+    if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
+      dwldLink.setAttribute("target", "_blank");
+    }
+    dwldLink.setAttribute("href", url);
+    dwldLink.setAttribute("download", "poradci.csv");
+    dwldLink.style.visibility = "hidden";
+    document.body.appendChild(dwldLink);
+    dwldLink.click();
+    document.body.removeChild(dwldLink);
   }
 
 }
--- src/app/smlouvy-table/smlouvy-table.component.ts
+++ src/app/smlouvy-table/smlouvy-table.component.ts
@@ -7,7 +7,7 @@ import { ApiService } from '../api.service';
   styleUrls: ['./smlouvy-table.component.css']
 })
 export class SmlouvyTableComponent implements OnInit {
-  smlouvy = [{ev_cislo: '', instituce: '', klient: '', spravce: '', dat_uzavreni: '', dat_platnosti: '', dat_ukonceni: ''}];
+  smlouvy = [{ev_cislo: '', instituce: '', klient: 0, spravce: 0, dat_uzavreni: '', dat_platnosti: '', dat_ukonceni: ''}];
 
   constructor(private api: ApiService) { }
 
@@ -22,6 +22,71 @@ export class SmlouvyTableComponent implements OnInit {
         error: (error) => (error.error['code'] == "token_not_valid") ? localStorage.removeItem('token') : ''
       }
     )
+  }
+
+  exportCsv() {
+    this.downloadFile(this.smlouvy);
+  }
+
+  strRep(data: any) {
+    if(typeof data == "string") {
+      let newData = data.replace(/,/g, " ");
+       return newData;
+    }
+    else if(typeof data == "undefined") {
+      return "-";
+    }
+    else if(typeof data == "number") {
+      return  data.toString();
+    }
+    else {
+      return data;
+    }
+  }
+
+  ConvertToCSV(objArray: string | { ev_cislo: string; instituce: string; klient: number; spravce: number; dat_uzavreni: string; dat_platnosti: string; dat_ukonceni: string; }[], headerList: string[]) {
+    console.log(objArray);
+    console.log(headerList);
+    let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+    let row = 'S.No,';
+
+    let newHeaders = ["ev_cislo", "instituce", "klient", "spravce", "dat_uzavreni", "dat_platnosti", "dat_ukonceni"];
+
+    for (let index in newHeaders) {
+      row += newHeaders[index] + ',';
+    }
+    row = row.slice(0, -1);
+    str += row + '\r\n';
+    for (let i = 0; i < array.length; i++) {
+      let line = (i + 1) + '';
+      for (let index in headerList) {
+        let head = headerList[index];
+
+        line += ',' + this.strRep(array[i][head]);
+      }
+      str += line + '\r\n';
+    }
+    return str;
+  }
+
+  downloadFile(data: { ev_cislo: string; instituce: string; klient: number; spravce: number; dat_uzavreni: string; dat_platnosti: string; dat_ukonceni: string; }[], filename = 'data') {
+    let arrHeader = ["ev_cislo", "instituce", "klient", "spravce", "dat_uzavreni", "dat_platnosti", "dat_ukonceni"];
+    let csvData = this.ConvertToCSV(data, arrHeader);
+    console.log(csvData)
+    let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
+    let dwldLink = document.createElement("a");
+    let url = URL.createObjectURL(blob);
+    let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+    if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
+      dwldLink.setAttribute("target", "_blank");
+    }
+    dwldLink.setAttribute("href", url);
+    dwldLink.setAttribute("download", "smlouvy.csv");
+    dwldLink.style.visibility = "hidden";
+    document.body.appendChild(dwldLink);
+    dwldLink.click();
+    document.body.removeChild(dwldLink);
   }
 
 }
