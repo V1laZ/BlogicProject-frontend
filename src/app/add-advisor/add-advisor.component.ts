216b1e29efb155ce4aa73ca8ef7bc0ed928a178a
V1laZ <pasikstudiocz@gmail.com>
2022-08-26 16:15:47 +0200

    delete unnecessary imports

--- src/app/add-advisor/add-advisor.component.ts
+++ src/app/add-advisor/add-advisor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { NgForm} from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
 
